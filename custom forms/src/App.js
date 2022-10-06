9a57cd76f6aae29ffcfa57ae7c49de68138eebe8
192.168.0.1 <90830312+negativexp@users.noreply.github.com>
2022-10-06 10:55:10 +0200

    asd

--- custom forms/src/App.js
+++ custom forms/src/App.js
@@ -1,21 +1,82 @@
+import { useState } from 'react';
 import './index.css';
 
 function App() {
 
-  function test_button() {
-    console.log(document.getElementById("description-input").value)
-  }
+  const [serviceList, setServiceList] = useState([{ service: "" }]);
+
+  const handleServiceChange = (e, index) => {
+    const { name, value } = e.target;
+    const list = [...serviceList];
+    list[index][name] = value;
+    setServiceList(list);
+  };
+
+  const handleServiceRemove = (index) => {
+    const list = [...serviceList];
+    list.splice(index, 1);
+    setServiceList(list);
+  };
+
+  const handleServiceAdd = () => {
+    setServiceList([...serviceList, { service: "" }]);
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>negativexp forms</h1>
-      <p>Name: <input type="text" id="name-input"/></p>
-      <p>Description: <textarea type="text" id="description-input"/></p>
-      
-
-      <button onClick={() => {test_button()}}>test button</button>
       </header>
+
+      <form className="App" autoComplete="off">
+        <div className="form-field">
+          <label htmlFor="service">Service(s)</label>
+          {serviceList.map((singleService, index) => (
+            <div key={index} className="services">
+              <div className="first-division">
+                <input
+                  name="service"
+                  type="text"
+                  id="service"
+                  value={singleService.service}
+                  onChange={(e) => handleServiceChange(e, index)}
+                  required
+                />
+                {serviceList.length - 1 === index && serviceList.length < 4 && (
+                  <button
+                    type="button"
+                    onClick={handleServiceAdd}
+                    className="add-btn"
+                  >
+                    <span>Add a Service</span>
+                  </button>
+                )}
+              </div>
+              <div className="second-division">
+                {serviceList.length !== 1 && (
+                  <button
+                    type="button"
+                    onClick={() => handleServiceRemove(index)}
+                    className="remove-btn"
+                  >
+                    <span>Remove</span>
+                  </button>
+                )}
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="output">
+          <h2>Output</h2>
+          {serviceList &&
+            serviceList.map((singleService, index) => (
+              <ul key={index}>
+                {singleService.service && <li>{singleService.service}</li>}
+              </ul>
+            ))}
+        </div>
+      </form>
+
     </div>
   );
 }
