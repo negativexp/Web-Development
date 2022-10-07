86bda985ebc5f4d1e1f10ba454743aa1e255d5ca
192.168.0.1 <90830312+negativexp@users.noreply.github.com>
2022-10-07 10:55:08 +0200

    s

--- custom forms/src/App.js
+++ custom forms/src/App.js
@@ -4,23 +4,55 @@ import './index.css';
 function App() {
 
   const [items, setItems] = useState([]);
+  const [question, setQuetions] = useState([]);
 
-  const HandleAdd = () => {
-    const old = [...items,[]]
-    setItems(old)
+  const HandleAddItems = () => {
+
+    var item = {
+      _value: "",
+    }
+    setItems(prev => [...prev, item])
+
+    // const old = [...items,[]]
+    // setItems(old)
   }
 
-  const HandleChange = (e, i) => {
+  function _HandleAddItems() {
+    var item = {
+      _value: "",
+    }
+    setItems(prev => [...prev, item])
+  }
+
+  const HandleChangeItems = (e, i) => {
     const inputData = [...items]
     inputData[i] = e.target.value;
     setItems(inputData)
   }
 
-  const HandleDelete = (i) => {
+  const HandleDeleteItems = (i) => {
     const deleteData = [...items]
     deleteData.splice(i,1)
     setItems(deleteData)
   }
+
+  const HandleAddQuestions = () => {
+    const old = [...question, []]
+    console.log(old)
+    setQuetions(old)
+  }
+
+  const HandleChangeQuestions = (e, i) => {
+    const inputData = [...question]
+    inputData[i] = e.target.value;
+    setQuetions(inputData);
+  }
+
+  function test_func() {
+    console.log(question)
+    console.log(items)
+  }
+
 
   return (
     <div className="App">
@@ -29,24 +61,43 @@ function App() {
         <h1>negativexp forms</h1>
       </header>
 
-      <button onClick={() => HandleAdd()}>Add</button>
+      <button onClick={() => _HandleAddItems()}>Add items</button>
+      <button onClick={() => HandleAddQuestions()}>add questions</button>
 
       {items.map((data, i) => {
         return (
           <div>
-            <input value={data} onChange={e => HandleChange(e, i)}/>
-            <button onClick={() => HandleDelete(i)}>x</button>
+            <input value={data} onChange={e => HandleChangeItems(e, i)}/>
+            <button onClick={() => HandleDeleteItems(i)}>x</button>
           </div>
         )
       })}
 
+      <button onClick={() => test_func()}>test</button>
+
       <div>
-        <h1>testy</h1>
+        <h1>Items</h1>
 
         {items.map((data ,i) => {
           return (
             <div>
               <h2>{i} ; {data}</h2>
+            </div>
+          )
+        })}
+
+        <h1>Questions</h1>
+        {question.map((data, i) => {
+          return (
+            <div>
+              <h2>{i} ; {data}</h2>
+              {items.map((data, i) => {
+                return (
+                  <div>
+                    <label>{data}</label>
+                  </div>
+                )
+              })}
             </div>
           )
         })}
