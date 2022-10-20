2e001bfbfbf5784a2cb620c363225ada79aee608
192.168.0.1 <90830312+negativexp@users.noreply.github.com>
2022-10-20 23:28:50 +0200

    hjvg

---
+++ components/components/src/App.js
@@ -0,0 +1,18 @@
+import { react } from "react";
+
+// components
+import WelcomeSign from "./components/WelcomeSign";
+
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <WelcomeSign/>
+    
+    </div>
+  );
+}
+
+export default App;
