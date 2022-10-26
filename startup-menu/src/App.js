fe7fc194045f354f8f383b80a2f1c2ac57d493c2
192.168.0.1 <90830312+negativexp@users.noreply.github.com>
2022-10-26 20:42:59 +0200

    asd

---
+++ startup-menu/src/App.js
@@ -0,0 +1,29 @@
+import "./css/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div class="">
+
+      <div class="outer-div">
+
+        <div class="head">
+          <h1>Newoutsider</h1>
+        </div>
+
+        <div class="body">
+          <h2>Head</h2>
+        </div>
+
+        <div class="foot">
+          <h1>Head</h1>
+        </div>
+
+      </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
