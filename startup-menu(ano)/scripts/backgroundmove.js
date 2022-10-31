0b9069e6eabba8262cbe17005891d25f6f6f3df8
192.168.0.1 <90830312+negativexp@users.noreply.github.com>
2022-10-31 10:55:46 +0100

    asd

--- startup-menu(ano)/scripts/backgroundmove.js
+++ startup-menu(ano)/scripts/backgroundmove.js
@@ -1,9 +1,20 @@
-const el = document.getElementById("module");
+// const el = document.getElementById("module");
+const el = document.querySelector('.background');
 
-window.addEventListener("mousemove", (e) => {
-    speed = 20;
-    x = -e.offsetX / speed;
-    y = -e.offsetY / speed;
+el.addEventListener("mousemove", (e) => {
+    let speed = 30;
+    const windowWidth = window.innerWidth / speed;
+    const windowHeight = window.innerHeight / speed
+    // x = -e.offsetX / speed;
+    // y = -e.offsetY / speed;
+
+    x = -e.clientX / windowWidth;
+    y = -e.clientY / windowHeight;
+
+    // console.log(x + " ; " + y);
+    
+    
+    // el.style.transform = `translate3d(-${x}%, -${y}%, 0)`;
     el.style.setProperty('--x', x + "px");
     el.style.setProperty('--y', y + "px");
 });
