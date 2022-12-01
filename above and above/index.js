4015b5028b903c26a80beab00669db2953fa31fd
192.168.0.1 <90830312+negativexp@users.noreply.github.com>
2022-12-01 21:54:45 +0100

    ....

--- above and above/index.js
+++ above and above/index.js
@@ -20,26 +20,23 @@ function sectionHandle(section) {
 
     if(section != lastSection) {
         if(section == 1) {
+            setZindex(sections[0])
             sections[0].className = "animationClass"
-            sections[0].style.zIndex = "99";
 
             sections.forEach(element => {
                 if(element != sections[0]) {
-                    element.style.zIndex = "1";
                     element.classList.remove("animationClass")
-                    element.style.top = "0"
                 }
             });
 
             lastSection = section
         }
         if(section == 2) {
+            setZindex(section)
             sections[1].className = "animationClass"
-            sections[1].style.zIndex = "99";
 
             sections.forEach(element => {
                 if(element != sections[1]) {
-                    element.style.zIndex = "1";
                     element.classList.remove("animationClass")
                 }
             });
@@ -47,6 +44,7 @@ function sectionHandle(section) {
             lastSection = section
         }
         if(section == 3) {
+            setZindex(section)
             sections[2].className = "animationClass"
             sections[2].style.zIndex = "99";
 
@@ -60,6 +58,7 @@ function sectionHandle(section) {
             lastSection = section
         }
         if(section == 4) {
+            setZindex(section)
             sections[3].className = "animationClass"
             sections[3].style.zIndex = "99";
 
@@ -73,6 +72,7 @@ function sectionHandle(section) {
             lastSection = section
         }
         if(section == 5) {
+            setZindex(section)
             sections[4].className = "animationClass"
             sections[4].style.zIndex = "99";
 
@@ -86,4 +86,17 @@ function sectionHandle(section) {
             lastSection = section
         }
     }
+}
+
+function setZindex(section) {
+    let counter = 0;
+
+    sections.map((element) => {
+        if(section.id != element.id) {
+            console.log(section.id, " false")
+        } else {
+            console.log(element.id, "true")
+        }
+        console.log(counter++)
+    })
 }
