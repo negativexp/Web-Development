2fdd1223eff0cdd8cd212b66df31846437cb5358
192.168.0.1 <90830312+negativexp@users.noreply.github.com>
2022-12-10 01:14:56 +0100

    personal webiste

---
+++ do something new/index.js
@@ -0,0 +1,103 @@
+mainMenuSVG = [
+    document.getElementById("svg1"),
+    document.getElementById("svg2"),
+    document.getElementById("svg3"),
+    document.getElementById("svg4"),
+    document.getElementById("svg5")
+]
+
+mainMenuSVG.forEach(element => {
+    element.style.opacity = 0
+});
+
+function startEverything(button) {
+    console.log("lets go.")
+
+    mainMenuSVG.forEach(element => {
+        element.style.opacity = 1
+    });
+
+    var tl = anime.timeline({
+        easing: "easeOutExpo",
+        duration: 1000
+    });
+
+    tl.add({
+        targets: button,
+        left: "-10%",
+        duration: 1000
+    })
+    .add({
+        targets: "#svg1",
+        strokeDashoffset: [anime.setDashoffset, 0],
+        easing: 'easeInOutSine',
+        duration: 1250,
+        direction: 'alternate',
+    })
+    .add({
+        targets: "#svg1",
+        opacity: 0,
+        duration: 250
+    })
+    .add({
+        targets: "#svg2",
+        strokeDashoffset: [anime.setDashoffset, 0],
+        easing: 'easeInOutSine',
+        duration: 1250,
+        direction: 'alternate',
+    })
+    .add({
+        targets: "#svg2",
+        opacity: 0,
+        duration: 250
+    })
+    .add({
+        targets: "#svg3",
+        strokeDashoffset: [anime.setDashoffset, 0],
+        easing: 'easeInOutSine',
+        duration: 1250,
+        direction: 'alternate',
+    })
+    .add({
+        targets: "#svg3",
+        opacity: 0,
+        duration: 250
+    })
+    .add({
+        targets: "#svg4",
+        strokeDashoffset: [anime.setDashoffset, 0],
+        easing: 'easeInOutSine',
+        duration: 2000,
+        direction: 'alternate',
+    })
+    .add({
+        targets: "#svg5",
+        opacity: [0, 1],
+        duration: 2000
+    })
+    .add({
+        targets: ".navbar ul li",
+        top: "-50px",
+        delay: function(el, i, l) {
+            return i * 100;
+        }
+    })
+}
+
+function pushDownOption(item) {
+    anime({
+        targets: item,
+        top: 0,
+        duration: 150,
+        easing: "easeInOutSine",
+    })
+}
+
+function pushUpOption(item) {
+    anime({
+        targets: item,
+        top: "-50px",
+        duration: 150,
+        easing: "easeInOutSine",
+    })
+}
